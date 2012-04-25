fc91bd2fc731dcf5088be1cd1aee97ec9bdbe283
Helge Wiemann <hw@digineo.de>
2012-04-25 15:58:43 +0200

    Helper-Display-Methode für das Rating. Generieren der Option-Texte aus den Locales

--- lib/rating_widget.rb
+++ lib/rating_widget.rb
@@ -4,8 +4,26 @@ module RatingWidget
   end
   
   module ViewHelper
-    def rating_tag(tag_id, option_texts, initial)
-      select_tag tag_id, options_for_select(option_texts, initial), :include_blank => true
+    def rating_input(tag_id, value, options={})
+      options[:max] = 5
+      select_tag tag_id, options_for_select(create_option_texts(options[:max]), value), :include_blank => true
+    end
+    
+    def rating_display(value, max=5)
+      rating_html = '<div class="ui-rating">'
+      max.times { |i|
+        value > i ? rating_html << '<div class="ui-rating-star ui-rating-full"></div>' : rating_html << '<div class="ui-rating-star ui-rating-empty"></div>'
+      }
+      rating_html << '</div>'
+      rating_html.html_safe
+    end
+    
+    def create_option_texts(max)
+      option_texts = Hash.new
+      max.times { |i|
+        option_texts[I18n.t("rating.#{i+1}")] = i+1
+      }
+      option_texts
     end
   end
   
